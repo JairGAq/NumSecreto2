37ef920a3b90aea51a2408773f6260cf0b5633c2
Jair <jair.guerreroaq@outlook.com>
2025-03-13 00:30:22 -0600

    funcion_Asignartextoelemento

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del Numero Secreto';
+// Autor: Jair Guerrero
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un numero del 1 al 10';
-
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+}
 function intentoDeUsuario(){
     alert('Click desde el boton')
 }
+
+asignarTextoElemento('h1', 'Juego del numero secreto!');
+asignarTextoElemento ('p', 'Indica un numero del 1 al 10');
