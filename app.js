c0bd5d0374cc10acc8f43ddfd56af9917245db6f
Jair <jair.guerreroaq@outlook.com>
2025-03-13 09:39:23 -0600

    ReiniciarJuego

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 1;
 
-
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -13,12 +12,20 @@ function verificarIntento(){
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Acertaste el numero en ${intentos} ${intentos === 1 ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else if (numeroDeUsuario > numeroSecreto){
         asignarTextoElemento('p','El numero secreto es menor')
     }else{
         asignarTextoElemento('p','El numero secreto es mayor')
     }
     intentos++;
+    limpiarCaja();
+    return;
+}
+
+function limpiarCaja(){
+    //document.getElementById('valorUsuario').value = '';
+    document.querySelector('#valorUsuario').value = '';
     return;
 }
 
@@ -26,5 +33,18 @@ function generarNumeroSecreto() {
     return Math.floor(Math.random() * 10)+1;
 }
 
-asignarTextoElemento('h1', 'Juego del numero secreto!');
-asignarTextoElemento ('p', 'Indica un numero del 1 al 10');
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del numero secreto!');
+    asignarTextoElemento ('p', 'Indica un numero del 1 al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarCaja(); //limpiar caja
+    condicionesIniciales(); //indicar mensaje de intervalo de numeros
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');//desabilitar el boton de nuevo juego
+    return;
+}
+
+condicionesIniciales();
